d729f4e86480848d4d1fd88ba73948871d1aa726
tomelsom <te28@hotmail.com>
2025-08-31 23:47:49 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'cookies/accept'
+  get 'cookies/reject'
+  get 'appointments/index'
+  root to: "pages#home"
+  get '/appointments', to: 'appointments#index'
+  post "/cookies/accept", to: "cookies#accept"
+  post "/cookies/reject", to: "cookies#reject"
+  get '/pessary_fitting', to: 'pages#about'
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :contacts, only: [:create]
+end
