df504a3929f61c1778ea3b1e65da21f8ebc3d2ba
tomelsom <te28@hotmail.com>
2025-09-01 23:07:23 +0100

    created home and about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+  before_action :assign_meta_tags
+
+  def home
+      @contact = Contact.new
+    end
+
+  def about
+    @contact = Contact.new
+  end
 end
