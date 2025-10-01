ad07bf0582b5622df9dbbea31c21763d762eab16
tomelsom <te28@hotmail.com>
2025-10-01 23:50:07 +0100

    update meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def assign_meta_tags
     set_meta_tags(
-      title: 'Absolute Health and Physio',
+      title: 'Richmond Physiotherapy - Sharon Edwards',
       description: "Expert physiotherapist with 30+ years of experience in musculoskeletal, sports, and pelvic health. Specializing in women's & men's health, sports injuries, and rural healthcare. Trained in Australia and UK, offering comprehensive physiotherapy services and innovative treatments.",
       keywords: 'Richmond Physiotherapy, Back pain, Neck pain, Knee pain, Osteoarthritis, Meniscus injury, Cruciate ligament tear, Muscle tear, Ligament tear, Foot injuries, Foot biomechanics, Orthotics, Pronation, Stress fracture, Postop, Rotator cuff tear, Pelvic organ prolapse, Bladder prolapse, Vaginal prolapse, Incontinence, Pelvic pain, Cystitis, Pessaries, Pelvic floor dysfunction, Pregnancy pain, Pelvic girdle pain, Antenatal physiotherapy, Post natal MOT',
       og: {
