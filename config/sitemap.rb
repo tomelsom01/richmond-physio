197daf695da4e451e1d39f042a3fe64d504accd6
tomelsom <te28@hotmail.com>
2025-10-18 23:40:06 +0100

    updated site map

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,34 +1,13 @@
+# config/sitemap.rb
+require 'rubygems'
+require 'sitemap_generator'
+
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "https://www.physiotherapyrichmond.co.uk"
 
+
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
-
-    # Home page
-    add root_path, priority: 1.0, changefreq: 'daily'
-
-    # About page
-    add pessary_fitting_path, priority: 0.7, changefreq: 'monthly'
-
+  add '/', changefreq: 'weekly', priority: 1.0
+  add '/pessary_fitting', changefreq: 'monthly', priority: 0.9
+  add '/contact', changefreq: 'monthly', priority: 0.8 if Rails.application.routes.url_helpers.respond_to?(:contact_path)
 end
